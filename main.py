5bad76630adc81e35f29a5ac3b36f7b206bec54d
Ben Lawrence <65065992+Bentheminernz@users.noreply.github.com>
2024-06-14 09:53:37 +1200

    remove credits, adds nz sea lion facts template

--- main.py
+++ main.py
@@ -26,10 +26,6 @@ def tuatara():
 def nzsealion():
     return render_template('animals/NZsealion.html')
 
-@app.route('/credits')
-def credits():
-    return render_template('other/credits.html')
-
 @app.route('/test')
 def test():
     return render_template('test.html')
