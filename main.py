943fc2e189b588f6c1bdfb44d85438afc9d901ef
Ben Lawrence <65065992+Bentheminernz@users.noreply.github.com>
2024-06-17 14:56:55 +1200

    Website should be finished now

--- main.py
+++ main.py
@@ -26,9 +26,5 @@ def tuatara():
 def nzsealion():
     return render_template('animals/NZsealion.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
